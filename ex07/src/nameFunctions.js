1327ca863db8d6a386549d2d2a1c36565a2f69a0
mbabic-arena <m_babic@teol.net>
2021-03-24 10:23:58 +0100

    Exercises for Day16

---
+++ ex07/src/nameFunctions.js
@@ -0,0 +1,11 @@
+//Only change code below this line
+const firstName = (firstName) => {
+  return firstName.toUpperCase();
+};
+const lastName = (lastName) => {
+  return lastName.toLowerCase();
+};
+
+exports.firstName = firstName;
+exports.lastName = lastName;
+//Only change code above this line
